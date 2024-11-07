08aff3d29a3feda9e12402db9473c1f16cb26e26
IMAM IHSANI <58213864+imamihsani@users.noreply.github.com>
2024-11-07 15:32:45 +0700

    Create MyQuery.sql

---
+++ MyQuery.sql
@@ -0,0 +1,16 @@
+-- UPDATE WHERE
+  UPDATE nama_database.`nama_tabel`
+  SET `kolom_yg_mau_diubah` = 'diganti ke ini'
+  WHERE `kolom_yg_mau_diubah` = 'ini';
+-- 
+
+-- BIKIN FOREIGN KEY DARI TABEL ANAK KE TABEL UTAMA
+  ALTER TABLE msa_web.win_proyek_kronologis -- tabel anak
+  ADD CONSTRAINT fk_proyek_kronologis
+  FOREIGN KEY (proyek_id) REFERENCES msa_web.win_proyek(proyek_id) -- ini tabel induk/utama
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
+  -- BATAL
+  ALTER TABLE msa_web.win_proyek_kronologis 
+  DROP FOREIGN KEY fk_kronologis_proyek;
+--

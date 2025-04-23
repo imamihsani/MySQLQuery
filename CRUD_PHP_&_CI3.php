93bc64cb5eb4dedbb3175052709be1516efb64d8
IMAM IHSANI <58213864+imamihsani@users.noreply.github.com>
2025-04-23 12:16:13 +0700

    Update CRUD_PHP_&_CI3.php
    tambah query cek

--- CRUD_PHP_&_CI3.php
+++ CRUD_PHP_&_CI3.php
@@ -34,6 +34,21 @@ public function getDataBuku() {
     return $DB->affected_rows();
   }
 
+public function cekboqalokasiproyek($proyek_id, $boq_id) {
+    $DB = $this->load->database('old_db', TRUE);
+    $sql = "
+        SELECT COUNT(*) as total 
+        FROM eng_proyek_boq_rekap 
+        WHERE proyek_id = ? 
+        AND boq_id = ?
+    ";
+    
+    $query = $DB->query($sql, [$proyek_id, $boq_id]);
+    $result = $query->row();
+    
+    return $result->total > 0;
+}
+
 
 
 -- CI3
@@ -69,4 +84,9 @@ public function getDataBuku() {
     $this->db->where('id', $id);
     $this->db->delete('buku');
     return $this->db->affected_rows();
+  }
+
+  public function cekboqalokasiproyek($proyek_id,$boq_id){
+    $DB2 = $this->load->database('old_db', TRUE);
+    return $DB2->where('proyek_id', $proyek_id)->where('boq_id', $boq_id)->get('eng_proyek_boq_rekap')->num_rows() > 0;
   }
